a7454938e97a7897a38ba2a224a21d02c3ff81ba
vv.golubkov <vv.golubkov@tensor.ru>
2024-05-18 12:49:12 +0300

    fix bug with delete and update CRUD

--- server/routes/records.js
+++ server/routes/records.js
@@ -3,18 +3,27 @@ const router = express.Router();
 
 const Record = require('../models/Record');
 
+//Get all data from db
 router.get('/', async (req, res) => {
     res.json(await Record.find());
 });
 
+//Add data to db
 router.post('/', async (req, res) => {
     const record = new Record(req.body);
     await record.save();
     res.json({state: 'success'});
 });
 
+//Update some data in db
+router.put('/:id', async (req, res) => {
+    await Record.findByIdAndUpdate(req.params.id, req.body);
+    res.json({state: 'updated'});
+ });
+
+ //Delete some data from db
 router.delete('/:id', async (req, res) => {
-    await Record.findByIdAndRemove(req.params.id);
+    await Record.findByIdAndDelete(req.params.id);
     res.json({state: 'deleted'});
 });
 
