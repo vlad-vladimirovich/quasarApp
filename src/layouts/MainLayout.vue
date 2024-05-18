fb4ee903e0e9bdc19e501128c88e103d76e0143a
vv.golubkov <vv.golubkov@tensor.ru>
2024-05-18 12:49:52 +0300

    remove unsed imports and page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -75,53 +75,11 @@
 
 <script setup>
 import { ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-import { defaults } from 'autoprefixer';
 import { date } from 'quasar';
 
 defineOptions({
   name: 'MainLayout'
 })
-
-const menuList = [
-  {
-    icon: 'inbox',
-    label: 'Inbox',
-    separator: true
-  },
-  {
-    icon: 'send',
-    label: 'Outbox',
-    separator: false
-  },
-  {
-    icon: 'delete',
-    label: 'Trash',
-    separator: false
-  },
-  {
-    icon: 'error',
-    label: 'Spam',
-    separator: true
-  },
-  {
-    icon: 'settings',
-    label: 'Settings',
-    separator: false
-  },
-  {
-    icon: 'feedback',
-    label: 'Send Feedback',
-    separator: false
-  },
-  {
-    icon: 'help',
-    iconColor: 'primary',
-    label: 'Help',
-    separator: false
-  }
-]
-
 const leftMenuOpener = ref(false)
 
 function toggleLeftDrawer () {
